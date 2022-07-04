081bad98d84f0ee26a3a95db43795b11cec4c5cd
JULIA MARTINS <juliartmartins@gmail.com>
2022-07-04 13:15:42 -0400

    Socket init config

---
+++ server.js
@@ -0,0 +1,49 @@
+'use restrict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+    res.render('index.html')
+})
+
+let messages = [];
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`);
+
+    socket.emit('previosMessages', messages);
+    
+    socket.on('sendMessage', data => {
+        console.log(data)
+        messages.push(data);
+        socket.broadcast.emit('recivedMessage', data);
+    })
+});
+
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+      return val;
+    }
+
+    if (port >= 0) {
+      return port;
+    }
+
+    return false;
+  }
+
+const port = normalizePort(process.env.PORT || '3000')
+server.listen(port);
